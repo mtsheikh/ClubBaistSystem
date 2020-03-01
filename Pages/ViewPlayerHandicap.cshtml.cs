f6f5859fc4c428783643158c79ebc1f9060f98c1
mtsheikh <mtsheikh@ualberta.ca>
2020-03-01 11:50:12 -0700

    implemented View Player Handicap

---
+++ Pages/ViewPlayerHandicap.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ClubBaistSystem.Domain;
+using ClubBaistSystem.TechnicalServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClubBaistSystem.Pages
+{
+    [Authorize]
+    [BindProperties]
+    public class ViewPlayerHandicap : PageModel
+    {
+        private readonly CBS _requestDirector = new CBS();
+
+        [TempData] public string Alert { get; set; }
+
+        public ClubBaistUser authenticatedPlayer { get; set; }
+        public decimal PlayerHandicapFactor { get; set; }
+        public void OnGet()
+        {
+            authenticatedPlayer = ClubBaistUsers.GetUserFromUserName(User.Identity.Name);
+            PlayerHandicapFactor = _requestDirector.ViewPlayerHandicap(authenticatedPlayer);
+        }
+    }
+}
